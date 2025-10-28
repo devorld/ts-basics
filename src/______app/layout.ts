042aadfeb88740fbeeb3cd7d93448dde284a6ee8
Gennadii M <jsdevlop@gmail.com>
2025-10-28 15:39:53 +0400

    feat(nav): moved site menu from `layout` to `shared/`

--- src/______app/layout.ts
+++ src/______app/layout.ts
@@ -1,36 +1,12 @@
 import { Router } from './router/Router.ts';
 
+import {siteNavigationMenu} from '../_shared/ui/site-navigation-menu.ts';
 import loadingSpinner from '../_shared/assets/animations/loading-spinner.gif';
 
 import './index.css';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-<nav>
-  <ul>
-    <li>
-      <a href="/">Index</a>
-    </li>
-    <li>
-        Patterns
-        <ul>
-          <li>
-            Behavioral
-          </li>
-          <li>
-            Creational
-          </li>
-          <li>
-            Structural
-            <ul>
-              <li>
-                <a href="/memoization">Memoization</a>
-              </li>
-            </ul>
-          </li>
-        </ul>
-    </li>
-  </ul>
-</nav>
+<nav>${siteNavigationMenu}</nav>
 <main>
   <img src="${loadingSpinner}" class="spinner" alt="Loading spinner" />
 </main>
---
+++ src/_shared/ui/site-navigation-menu.ts
@@ -0,0 +1,28 @@
+const siteNavigationMenu = `
+  <ul>
+    <li>
+      <a href="/">Index</a>
+    </li>
+    <li>
+        <strong>Patterns</strong>
+        <ul>
+          <li>
+            <strong>Behavioral</strong> <small>- коммуникации между компонентами</small>
+          </li>
+          <li>
+            <strong>Creational</strong> <small>- гибкое создание компонентов</small>
+          </li>
+          <li>
+            <strong>Structural</strong> <small>- зависимости между компонентами</small>
+            <ul>
+              <li>
+                <a href="/memoization">Memoization <small>by closure idiomatic solution</small></a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+    </li>
+  </ul>
+`;
+
+export { siteNavigationMenu };
